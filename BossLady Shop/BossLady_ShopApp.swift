b6791fc8828c20c4d076c08cb90c4f9e3ec053f9
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-09-08 23:48:16 +0200

    Initial Commit

---
+++ BossLady Shop/BossLady_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BossLady_ShopApp.swift
+//  BossLady Shop
+//
+//  Created by Sergei Saliukov on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BossLady_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
